02dd34ea0123ca4d64e743a5ff989c1f225338dd
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-03-15 19:41:50 +0100

    Initial restructuring

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = (req, res) => {
-  res.end("Welcome to Micro");
-};
+const { json } = require('micro')
+
+const { connectDb } = require('./db-service')
+
+module.exports = async (req, res) => {
+  // parse the request body
+  const body = await json(req)
+
+  // TODO: validate the parameters
+
+  // setup a database connection
+  const client = connectDb()
+
+  // TODO: fetch the relevant row from the database
+  console.log(body)
+
+  // TODO: return the poster url
+  return {
+    hello: 1,
+  }
+}
