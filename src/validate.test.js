fdc51e1774e20e43bcc6c27cf4ce3cfe3f18f7fb
Nik Zaugg <zauggnik@gmail.com>
2020-03-17 18:09:24 +0100

    update tests

--- src/validate.test.js
+++ src/validate.test.js
@@ -1,6 +1,6 @@
 const test = require("ava");
 
-const validate = require("./validate");
+const { validateId } = require("./validate");
 
 const invalidFormatErrorMsg = `The passed id is not valid or has the wrong format. A correct id looks like this: tt3896198, two t followed by 7 digits`;
 const noIdErrorMsg = `No id parameter passed in the url.`;
@@ -8,14 +8,14 @@ const noIdErrorMsg = `No id parameter passed in the url.`;
 test("validate correct format", t => {
   const input = "tt3896198";
 
-  t.notThrows(() => validate(input));
+  t.notThrows(() => validateId(input));
 });
 
 test("validate undefined input", t => {
   const input = undefined;
 
   const expected = noIdErrorMsg;
-  const error = t.throws(() => validate(input));
+  const error = t.throws(() => validateId(input));
 
   t.is(error.message, expected);
 });
@@ -24,7 +24,7 @@ test("validate wrong input format (id starting with xx)", t => {
   const input = "xx3896198";
 
   const expected = invalidFormatErrorMsg;
-  const error = t.throws(() => validate(input));
+  const error = t.throws(() => validateId(input));
 
   t.is(error.message, expected);
 });
@@ -33,7 +33,7 @@ test("validate wrong input format (id ending with letters)", t => {
   const input = "tt38hello";
 
   const expected = invalidFormatErrorMsg;
-  const error = t.throws(() => validate(input));
+  const error = t.throws(() => validateId(input));
 
   t.is(error.message, expected);
 });
